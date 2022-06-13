77baa1fac35e6af961807bb01461e3ef5ded69c3
Numb4r <yuri.d.d.faria@gmail.com>
2022-06-13 00:42:40 -0300

    update parser

--- parser_.py
+++ parser_.py
@@ -1,17 +1,56 @@
-import const as const
+from const import TypeToken as tt
+from const import _sintaxGraph as sg
+from Token import validateTKHEAD
 from scanner import Scanner
+
 
 class Parser:
     def __init__(self,scanner:Scanner):
         self.sc = scanner
     def analysis(self):
         token = self.sc.nextToken()
+        
+        # error = 1
         while token != None:
+            # print(token)
+            # Forma burra,arrumar depois
+            if token.tipo == tt.TK_OPHEAD:
+                token = self.sc.nextToken()
+                pass
+                # TODO: Corrigir bug
+                # print(validateTKHEAD(token))
+                    
+                
+            #     token = self.sc.nextToken()
+            #     print(token)
+            #     if token.tipo == tt.TK_IDENTIFIER and token.lexeme == "include":
+            #         token =  self.sc.nextToken() 
+            #         print(token)
+            #         if token.tipo == tt.TK_OPLT:
+            #             token =  self.sc.nextToken() 
+            #             print(token)
+            #             if token.tipo == tt.TK_IDENTIFIER:
+            #                 token =  self.sc.nextToken() 
+            #                 print(token)
+            #             if token.tipo == tt.TK_OPDOT :
+            #                 token =  self.sc.nextToken() 
+            #                 print(token)
+            #                 if token.tipo == tt.TK_IDENTIFIER:
+            #                     token =  self.sc.nextToken() 
+            #                     print(token)
+            #                     if token.tipo == tt.TK_OPGT:
+            #                         token =  self.sc.nextToken() 
+            #                         print(token)
+            #                         error = 0
+            # if error == 1:
+            #     raise "Sintax Error"
+
+
+                
             nextToken = self.sc.nextToken()
-            print(token," ",nextToken)
+            print(token,nextToken)
             if nextToken != None:
-                # print(const._sintaxGraph.get(token.tipo))
-                if nextToken.tipo not in const._sintaxGraph.get(token.tipo):
+                if nextToken.tipo not in sg.get(token.tipo):
                     raise "Sintax Error"
             token = nextToken
 
