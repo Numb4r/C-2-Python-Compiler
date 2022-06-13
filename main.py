416ef637285a48b4eca8fc848d020a86b78947e4
Numb4r <yuri.d.d.faria@gmail.com>
2022-06-13 00:43:02 -0300

    update main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from scanner import Scanner
+from parser_ import Parser
+string = "newTestes/Código " 
+# string = "código"
+for i in range(1,10):
+    print(i)
+    sc = Scanner("./testes/"+string+str(i)+".txt",False)
+    # sc = Scanner("./testes/base.c")
+    # tk = sc.nextToken()
+    # while tk != None:
+    #     print(tk)
+    #     tk  = sc.nextToken()
+    pa = Parser(sc)
+    pa.analysis()
 
-sc = Scanner("a.c")
-token = sc.nextToken()
-if token == None:
-    print("Error")
-while  token != None:
-    print(token)
-    token = sc.nextToken()
-    
