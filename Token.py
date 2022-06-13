15cdd81c4bf51b0fa821f49af2d47f42a9c90968
Numb4r <yuri.d.d.faria@gmail.com>
2022-06-13 00:42:24 -0300

    update token class

--- Token.py
+++ Token.py
@@ -1,13 +1,18 @@
 import const
 
 class Token:
-    def __init__(self,tipo,lexeme=None,valor = None):
-        self.lexeme = lexeme
+    def __init__(self,tipo,lexeme=None,valor=None):
         self.tipo = tipo
+        self.lexeme = lexeme
         self.valor = valor
+        
     def __str__(self):
         string = "<"+str(self.tipo)
         string += ","+self.lexeme if self.lexeme != None else ""
         string += ","+self.valor if self.lexeme != None and self.valor !=None else ""
         string += ">"
         return string
+def validateTKHEAD(token:Token):
+    print(token.valor)
+    if token.lexeme == "include":
+        return re.match("<.*>|\".*\"",token.valor)
